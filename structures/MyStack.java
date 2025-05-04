d3c03d7b0537a00bc26709e600617bc76b5549ec
khushipanwar16 <khushiiipanwar16@gmail.com>
2025-05-04 21:19:27 +0530

    mystack updated

--- structures/MyStack.java
+++ structures/MyStack.java
@@ -1,9 +1,7 @@
 package structures;
 
-// Custom Stack class (LIFO: Last In First Out)
 public class MyStack<T> {
 
-    // Node class inside the Stack
     private class Node {
         T data;
         Node next;
@@ -14,24 +12,21 @@ public class MyStack<T> {
         }
     }
 
-    private Node top;   // The top item of the stack
-    private int size;   // How many items are in the stack
+    private Node top;   
+    private int size;   
 
-    // Constructor: When the stack is created, it's empty
     public MyStack() {
         this.top = null;
         this.size = 0;
     }
 
-    // Push: Add item onto the top of the stack
     public void push(T data) {
         Node newNode = new Node(data);
-        newNode.next = top;  // New node points to the current top
-        top = newNode;       // New node becomes the new top
+        newNode.next = top;  
+        top = newNode;      
         size++;
     }
 
-    // Pop: Remove and return the top item
     public T pop() {
         if (isEmpty()) {
             System.out.println("❗ Stack is empty. Cannot pop.");
@@ -39,12 +34,11 @@ public class MyStack<T> {
         }
 
         T data = top.data;
-        top = top.next;  // Move top to next node
+        top = top.next; 
         size--;
         return data;
     }
 
-    // Peek: Just look at the top item without removing it
     public T peek() {
         if (isEmpty()) {
             System.out.println("❗ Stack is empty. Nothing to peek.");
@@ -54,17 +48,14 @@ public class MyStack<T> {
         return top.data;
     }
 
-    // Check if the stack is empty
     public boolean isEmpty() {
         return size == 0;
     }
 
-    // Get the number of items in the stack
     public int size() {
         return size;
     }
 
-    // Display all items in stack (from top to bottom)
     public void display() {
         Node current = top;
         while (current != null) {
